0794f9ebc9dffd047bbe2c099af624654672ea3b
Anti-Code <mhmtgrdl32@gmail.com>
2017-06-10 22:46:20 +0300

    temporary config files

---
+++ test_config.py
@@ -0,0 +1,20 @@
+
+"""
+Define configuration variables in here
+"""
+DEBUG = True
+TESTING = True
+
+# tokens
+SECRET_KEY = "your secret key"
+
+# database
+SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# maps api
+GOOGLEMAPS_KEY = 'your api key'
+
+# forecast api
+FORECAST_API_URL = "your api url"
+FORECAST_API_KEY = "your api key"
